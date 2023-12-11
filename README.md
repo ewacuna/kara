44787f811ad4d828266ff7dc594833a58efb9036
Elvin Acuña <elvinwaz@outlook.com>
2023-12-10 18:22:12 -0600

    Remove files for deployment

--- README.md
+++
@@ -1,5 +0,0 @@
-# Kara
-
-A digital marketing agency website built with [Bootstrap 5](https://getbootstrap.com/).
-
-#### :sparkles: [See Demo](https://ewacuna.github.io/kara/)
--- src/index.js
+++
@@ -1,8 +0,0 @@
-// Styles
-import './styles.scss';
-import '../node_modules/tiny-slider/dist/tiny-slider.css';
-
-// Scripts
-import '../node_modules/@popperjs/core/dist/umd/popper.min.js';
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
-import './js/script.js';
